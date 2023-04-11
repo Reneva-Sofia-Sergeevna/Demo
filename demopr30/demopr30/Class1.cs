ebe37de3d0d1ca503a0b763c553fd4fb8d584d43
Nikita Orehov <biodragon203@gmail.com>
2023-04-11 10:33:29 +0300

    demo

---
+++ demopr30/demopr30/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demopr30
+{
+    internal class Class1
+        //привязка бд к wpf
+    {
+        private static user43Entities _db;
+        public static user43Entities db 
+        { get 
+            { 
+                return _db; 
+            } 
+        }
+
+    }
+}
